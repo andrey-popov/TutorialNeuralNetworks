3a6ceec58a40ca33c977a95db763932cad9121fc
Andrey Popov <rainfinder@gmail.com>
2015-02-04 20:31:30 +0100

    Add a script that users will be changing during the tutorial

---
+++ tutorial/train.C
@@ -0,0 +1,38 @@
+{
+    // STEP 1: Create a TMVA factory
+    
+    
+    // Read signal and background trees from source files
+    TString srcDirectory("../data/");
+    
+    TFile sgnTrainFile(srcDirectory + "tH_10k.root");
+    TFile sgnTestFile(srcDirectory + "tH_5k.root");
+    TFile bkgTrainFile(srcDirectory + "ttbar_10k.root");
+    TFile bkgTestFile(srcDirectory + "ttbar_5k.root");
+    
+    TTree *sgnTrainTree = (TTree *) sgnTrainFile.Get("Vars");
+    TTree *sgnTestTree = (TTree *) sgnTestFile.Get("Vars");
+    TTree *bkgTrainTree = (TTree *) bkgTrainFile.Get("Vars");
+    TTree *bkgTestTree = (TTree *) bkgTestFile.Get("Vars");
+    
+    
+    // STEP 2: Feed trees with signal and background events to the TMVA factory
+    
+    
+    // STEP 3: Specify input variables and per-event weights
+    
+    
+    // STEP 4: Command the TMVA factory to copy and process the trees
+    
+    
+    // STEP 5: Book an NN trained with backpropagation
+    
+    // STEP 7: Book an NN trained with BFGS algorithm and weight decay. Also book a projective
+    //likelihood discriminator
+    
+    
+    // STEP 6: Train the NN and run testing for it
+    
+    
+    return 0;
+}
